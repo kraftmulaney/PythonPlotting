fff60caaf2da64b56eb95b47eab6b29a3ad35baa
Ido <idoben2@hotmail.com>
2020-11-21 20:47:03 -0800

    Draw a pie chart

---
+++ main.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+def DrawPieChart():
+    # defining labels 
+    activities = ['eat', 'sleep', 'work', 'play'] 
+  
+    # portion covered by each label 
+    slices = [3, 7, 8, 6] 
+    
+    # color for each label 
+    colors = ['r', 'y', 'g', 'b'] 
+    
+    # plotting the pie chart 
+    plt.pie(slices, labels = activities, colors=colors,  
+            startangle=90, shadow = True, explode = (0, 0, 0.1, 0), 
+            radius = 1.2, autopct = '%1.1f%%') 
+    
+    # plotting legend 
+    plt.legend() 
+    
+    # showing the plot 
+    plt.show()
+
+
+
+# **** MAIN **************
+
+DrawPieChart()
